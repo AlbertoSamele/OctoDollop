952b29a3585014e4d2db615db5cfe1c2a80339f6
Alberto Samele <>
2021-11-28 20:01:19 +0100

    Project init

---
+++ ios/OctoDollop/OctoDollop/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OctoDollop
+//
+//  Created by alber848 on 28/11/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .red
+    }
+
+
+}
+
